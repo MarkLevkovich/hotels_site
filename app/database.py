31b7ecf5854f0ec4c3428bd042f197927309d8b3
MarkLevkovich <levmarkpost@gmail.com>
2025-08-21 19:13:31 +0300

    Init commit with ready BD

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+
+
+
+
+DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
