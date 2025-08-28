6a442057facbae757b872b00d79b0b1c18b12072
MarkLevkovich <levmarkpost@gmail.com>
2025-08-28 20:20:24 +0300

    Logout & Info routers added

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -15,4 +15,5 @@ router = APIRouter(
 
 @router.get('/')
 async def get_bookings(user: Users = Depends(get_current_user)):
-    return await BookingsDAO.find_all(user_id=1)
+    return await BookingsDAO.find_all(user_id=user.id)
+
