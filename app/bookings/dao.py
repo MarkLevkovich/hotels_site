10f3798f2ba30301c3e79d119263beeadfda0f0c
MarkLevkovich <levmarkpost@gmail.com>
2025-08-31 21:53:18 +0300

    Start create add-bookings router

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,7 +1,31 @@
+from datetime import date
 from app.database import async_session_maker
-from sqlalchemy import select
+from sqlalchemy import select, and_, or_, func
 from app.bookings.models import Bookings
 from app.dao.base import BaseDAO
+from app.hotels.models import Room
+
 
 class BookingsDAO(BaseDAO):
-    model = Bookings 
+    model = Bookings
+
+    @classmethod
+    async def add(cls, room_id: int, date_from: date, date_to: date):
+        booked_rooms = select(Bookings).where(
+            and_(
+                Bookings.room_id==room_id,
+                or_(
+                    and_(
+                        Bookings.date_from >= date_from,
+                        Bookings.date_from <= date_to
+                    ),and_(
+                        Bookings.date_from <= date_from,
+                        Bookings.date_to > date_from
+                    )
+                )
+            )
+        ).cte('booked_rooms')
+
+        rooms_left = select(
+            (Room.quantity - func.count(booked_rooms.room.id)).label('rooms_left')
+        )
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import APIRouter, Depends, Form, Response, Request
 from sqlalchemy import select, delete, update
 from typing import Annotated
@@ -16,4 +18,8 @@ router = APIRouter(
 @router.get('/')
 async def get_bookings(user: Users = Depends(get_current_user)):
     return await BookingsDAO.find_all(user_id=user.id)
+
+@router.post('/')
+async def add_bookings(room_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)):
+    await BookingsDAO.add(user.id, room_id, date_from, date_to)
 
