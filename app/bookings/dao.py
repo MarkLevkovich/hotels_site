a90e586145b655d1d52c5ec9bd4aec8a9c619328
MarkLevkovich <levmarkpost@gmail.com>
2025-09-01 21:07:46 +0300

    Rooms_left!

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -10,22 +10,29 @@ class BookingsDAO(BaseDAO):
     model = Bookings
 
     @classmethod
-    async def add(cls, room_id: int, date_from: date, date_to: date):
-        booked_rooms = select(Bookings).where(
-            and_(
-                Bookings.room_id==room_id,
-                or_(
+    async def add(cls, user_id: int, room_id: int, date_from: date, date_to: date):
+        async with async_session_maker() as session:
+                booked_rooms = select(Bookings).where(
                     and_(
-                        Bookings.date_from >= date_from,
-                        Bookings.date_from <= date_to
-                    ),and_(
-                        Bookings.date_from <= date_from,
-                        Bookings.date_to > date_from
+                        Bookings.room_id==room_id,
+                        or_(
+                            and_(
+                                Bookings.date_from >= date_from,
+                                Bookings.date_from <= date_to
+                            ),and_(
+                                Bookings.date_from <= date_from,
+                                Bookings.date_to > date_from
+                            )
+                        )
                     )
-                )
-            )
-        ).cte('booked_rooms')
+                ).cte('booked_rooms')
 
-        rooms_left = select(
-            (Room.quantity - func.count(booked_rooms.room.id)).label('rooms_left')
-        )
+                rooms_left = select(
+                    (Room.quantity - func.count(booked_rooms.c.room_id)).label('rooms_left')
+                    ).select_from(Room).join(
+                        booked_rooms, booked_rooms.c.room_id == Room.id
+                    ).where(Room.id==room_id).group_by(
+                        Room.quantity, booked_rooms.c.room_id
+                    )
+                rooms_left = await session.execute(rooms_left)
+                print(rooms_left.scalar())
