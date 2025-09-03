6a19e0d0face62fa59245c9d804f197018495bbe
MarkLevkovich <levmarkpost@gmail.com>
2025-09-03 20:58:17 +0300

    start doing task1

--- app/main.py
+++ app/main.py
@@ -5,10 +5,12 @@ from app.users.router import router as router_users
 app = FastAPI()
 
 @app.get('/')
-def hello() -> list:
-    return {
-        'Hello in my web-site!'
-    }
+def hello() -> str:
+    return 'Hello in my web-site!'
+
 
 app.include_router(router_users)
 app.include_router(router_bookings)
+
+
+# https://www.canva.com/design/DAGx8IbwuWk/4pcQFnR1H_HfGgfjIxoFaQ/view?utm_content=DAGx8IbwuWk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h3dd5ee65c5
