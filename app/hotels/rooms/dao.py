c2d332d78667b3596ca900479f299d54dc74c4ff
MarkLevkovich <levmarkpost@gmail.com>
2025-09-09 21:31:07 +0300

    added check booked rooms

--- app/hotels/rooms/dao.py
+++ app/hotels/rooms/dao.py
@@ -1,17 +1,31 @@
+from datetime import date
+
 from app.bookings.models import Bookings
 from app.dao.base import BaseDAO
 from app.database import async_session_maker
 from app.hotels.models import Room
-from sqlalchemy import select, insert, delete
-
+from sqlalchemy import select, insert, delete, and_, or_, func
+from app.bookings.models import Bookings as Booking
 
 
 class RoomsDAO(BaseDAO):
     model = Room
 
     @classmethod
-    async def show_rooms(cls, hotel_id: int):
+    async def show_rooms(cls, hotel_id: int, date_from: date ,date_to: date):
         async with async_session_maker() as session:
-            query = select(Room).where(Room.hotel_id==hotel_id)
-            res = await session.execute(query)
-            return res.scalars().all()
+            booked_rooms = (
+                select(Bookings.room_id)
+                .where(
+                    and_(
+                        Bookings.date_from <= date_to,
+                        Booking.date_to >= date_from
+                    )
+                )
+                .scalar_subquery()
+            )
+
+            available_rooms = select(Room).where(Room.hotel_id==hotel_id, Room.id.not_in(booked_rooms))
+
+            result = await session.execute(available_rooms)
+            return result.scalars().all()
--- app/hotels/rooms/router.py
+++ app/hotels/rooms/router.py
@@ -1,8 +1,10 @@
+from datetime import date
+
 from app.hotels.rooms.dao import RoomsDAO
 from app.hotels.router import router
 
 
 
 @router.get('/{hotel_id}/rooms')
-async def get_rooms(h_id: int):
-    return await RoomsDAO.show_rooms(hotel_id=h_id)
+async def get_rooms(h_id: int, d_from: date ,d_to: date):
+    return await RoomsDAO.show_rooms(hotel_id=h_id, date_from=d_from, date_to=d_to)
