c947a9c6dfd8afa1c533632a55bb538b63f1c5d8
MarkLevkovich <levmarkpost@gmail.com>
2025-09-11 21:07:35 +0300

    added simple hotel photos to front

---
+++ app/images/router.py
@@ -0,0 +1,10 @@
+import shutil
+
+from fastapi import APIRouter, UploadFile
+
+router = APIRouter(prefix='/images', tags=['Upload IMG'])
+
+@router.post('/hotels')
+async def add_hotel_image(name: int, file: UploadFile):
+    with open(f'app/static/images/{name}.webp', 'wb+') as file_obj:
+        shutil.copyfileobj(file.file, file_obj)
--- app/main.py
+++ app/main.py
@@ -3,10 +3,15 @@ from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
+from app.pages.router import router as router_pages
+from app.images.router import router as router_images
+from fastapi.staticfiles import StaticFiles
 
 
 
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='app/static'), 'static')
+
 
 @app.get('/')
 def hello() -> str:
@@ -17,6 +22,9 @@ app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
+app.include_router(router_pages)
+app.include_router(router_images)
 
 
-# https://www.canva.com/design/DAGx8IbwuWk/4pcQFnR1H_HfGgfjIxoFaQ/view?utm_content=DAGx8IbwuWk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h3dd5ee65c5
+
+
