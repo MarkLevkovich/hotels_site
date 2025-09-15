dc63832a630c102e9e7722593f6da2e577524bd1
MarkLevkovich <levmarkpost@gmail.com>
2025-09-15 21:18:34 +0300

    added email booking-check

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.bookings.models import Bookings
 from app.bookings.dao import BookingsDAO
 from app.bookings.schemas import SBookings
+from app.tasks.tasks import send_booking_confirmation
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 from app.exeptions import NoRoomException, NoBookingException
@@ -19,11 +20,32 @@ router = APIRouter(
 async def get_bookings(user: Users = Depends(get_current_user)):
     return await BookingsDAO.find_all(user_id=user.id)
 
+
 @router.post('/add_booking')
-async def add_bookings(room_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)):
+async def add_bookings(
+        room_id: int,
+        date_from: date,
+        date_to: date,
+        user: Users = Depends(get_current_user)
+):
     booking = await BookingsDAO.add(user.id, room_id, date_from, date_to)
     if not booking:
         raise NoRoomException
+    else:
+
+        booking_dict = {
+            "id": booking.id,
+            "user_id": booking.user_id,
+            "room_id": booking.room_id,
+            "date_from": booking.date_from.isoformat(),
+            "date_to": booking.date_to.isoformat(),
+            "price": float(booking.price) if booking.price else 0.0,
+            "total_cost": booking.total_price,
+            "total_days": booking.total_days
+
+        }
+        send_booking_confirmation.delay(booking_dict, user.email)
+        return booking
 
 @router.get('/del')
 async def del_bookings(b_id: int, user: Users = Depends(get_current_user)):
--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,6 +1,10 @@
+from app.config import settings
 from app.tasks.celery import celery
 from PIL import Image
 from pathlib import Path
+from pydantic import EmailStr
+import smtplib
+from app.tasks.email_templates import create_booking_confirmation
 
 
 @celery.task
@@ -11,3 +15,17 @@ def process_img(
     im = Image.open(im_path)
     im_resized_200_100 = im.resize((200, 100))
     im_resized_200_100.save(f'app/static/images/resized_200_100{im_path.name}')
+
+
+
+@celery.task
+def send_booking_confirmation(
+        booking: dict,
+        email_to: EmailStr,
+):
+    test_my_mail = settings.SMTP_USER
+    msg_content = create_booking_confirmation(booking, test_my_mail)
+
+    with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT) as server:
+        server.login(settings.SMTP_USER, settings.SMTP_PASS)
+        server.send_message(msg_content)
